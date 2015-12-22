db9e708db4d37d7031aad7a5640ab0347ed0895e
rastiin <rastin45@users.noreply.github.com>
2015-12-22 08:51:22 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {176229065,172767635,0,tonumber(our_id)},--Sudo users
+    sudo_users = {54213663},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
